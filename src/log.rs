1a12949d1356c11d943c1d96a44a27aec2203ac5
David Malchin <malchin459@gmail.com>
2024-06-14 21:12:09 +0300

    Log the level itself for all log levels

--- src/log.rs
+++ src/log.rs
@@ -3,14 +3,10 @@ use log::{Level, LevelFilter};
 pub fn init(filter: LevelFilter) -> anyhow::Result<()> {
     fern::Dispatch::new()
         .format(move |out, message, record| {
-            let mut prefix = "".to_owned();
-
-            if record.level() <= Level::Warn {
-                prefix = get_level_name(record.level());
-                prefix.push_str(": ");
-            }
-
-            out.finish(format_args!("{prefix}{message}"));
+            out.finish(format_args!(
+                "{}: {message}",
+                get_level_name(record.level()),
+            ));
         })
         .level(filter)
         .chain(std::io::stdout())
@@ -19,7 +15,7 @@ pub fn init(filter: LevelFilter) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn get_level_name(level: log::Level) -> String {
+fn get_level_name(level: Level) -> String {
     let s = match level {
         Level::Error => "error",
         Level::Warn => "warning",
