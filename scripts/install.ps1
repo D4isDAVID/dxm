e464057121d89c21da505474ca70efaecf961dc4
David Malchin <malchin459@gmail.com>
2026-05-19 01:13:53 +0300

    Fix powershell quick install script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,60 +1,55 @@
-param (
-    [Parameter()]
-    [ValidateNotNullOrEmpty()]
-    [string] $Release = "latest",
+function Info {
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [Object] $Object
+    )
 
-    [Parameter()]
-    [ValidateNotNullOrEmpty()]
-    [string] $InstallDir,
+    Write-Host "info: $Object"
+}
 
-    [Parameter()]
-    [switch] $NoEnvPath
-)
-begin {
-    function Info {
-        [CmdletBinding()]
-        param (
-            [Parameter(Mandatory, Position = 0)]
-            [Object] $Object
-        )
-        process {
-            Write-Host "info: $Object"
+function Error {
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [Object] $Object
+    )
+
+    [Console]::Error.WriteLine("error: $Object")
+    exit 1
+}
+
+function Ensure-Dependencies {
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string[]] $Dependencies
+    )
+
+    foreach ($dep in $Dependencies) {
+        if (!(Get-Command $dep -ErrorAction SilentlyContinue)) {
+            Error "'$dep' is not installed or available"
         }
     }
+}
 
+function Arg-Value {
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [string] $Key,
 
-    function Error {
-        [CmdletBinding()]
-        param (
-            [Parameter(Mandatory, Position = 0)]
-            [Object] $Object
-        )
-        process {
-            [Console]::Error.WriteLine("error: $Object")
-            exit 1
-        }
+        [Parameter(Position = 1)]
+        [string] $Value
+    )
+
+    if (!($Value)) {
+        Error "no value provided for argument '$Key'"
     }
 
+    return $Value
+}
 
-    function Ensure-Dependencies {
-        [CmdletBinding()]
-        param (
-            [Parameter(Mandatory, Position = 0)]
-            [string[]] $Dependencies
-        )
-        process {
-            foreach ($dep in $Dependencies) {
-                if (!(Get-Command $dep -ErrorAction SilentlyContinue)) {
-                    Error "'$dep' is not installed or available"
-                }
-            }
-        }
-    }
+$downloadDir = Join-Path ([System.IO.Path]::GetTempPath()) (New-Guid)
+New-Item -ItemType Directory -Path $downloadDir > $null
 
-
-    $downloadDir = Join-Path ([System.IO.Path]::GetTempPath()) (New-Guid)
-    New-Item -ItemType Directory -Path $downloadDir > $null
-
+try {
     $program = "dxm"
     $repository = "D4isDAVID/dxm"
 
@@ -81,16 +76,6 @@ begin {
         Error "unsupported architecture '$arch'"
     }
 
-    $programArgs = @()
-
-    if ($PSBoundParameters.ContainsKey('InstallDir')) {
-        $env:DXM_HOME = $InstallDir
-        [System.Environment]::SetEnvironmentVariable("DXM_HOME", $InstallDir, "User")
-    }
-    if ($NoEnvPath) {
-        $programArgs.Add("--no-env-path")
-    }
-
     $headers = @{
         "X-GitHub-Api-Version" = "2022-11-28"
     }
@@ -99,15 +84,9 @@ begin {
         Info "using provided GITHUB_PAT for authentication"
         $headers["Authorization"] = "token $env:GITHUB_PAT"
     }
-}
-process {
-    if ($Release -eq "latest") {
-        Info "fetching latest release"
-        $apiUrl = "https://api.github.com/repos/$repository/releases/latest"
-    } else {
-        Info "fetching release $Release"
-        $apiUrl = "https://api.github.com/repos/$repository/releases/tags/$Release"
-    }
+
+    Info "fetching latest release"
+    $apiUrl = "https://api.github.com/repos/$repository/releases/latest"
 
     try {
         $releaseJson = Invoke-RestMethod -Uri $apiUrl -Headers $headers -ErrorAction Stop
@@ -147,8 +126,9 @@ process {
         & chmod +x $binaryFile
     }
 
-    & $binaryFile self setup $programArgs
-}
-end {
+    & $binaryFile self setup
+} catch {
+    Error "unhandled error: $_"
+} finally {
     Remove-Item -Path $downloadDir -Recurse -Force -ErrorAction Ignore
 }
